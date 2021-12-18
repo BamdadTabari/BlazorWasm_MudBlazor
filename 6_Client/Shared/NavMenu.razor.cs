75d793e4c235929ea3f2b54de7b684df045de809
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-18 10:26:09 -0800

    cleaning

---
+++ 6_Client/Shared/NavMenu.razor.cs
@@ -0,0 +1,84 @@
+﻿using illegible.Client.Extensions.TelerikBlazorExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telerik.Blazor.Components;
+
+namespace illegible.Client.Shared
+{
+    public partial class NavMenu
+    {
+        #region Drawer Data
+
+        public TelerikDrawer<DrawerItem> Drawer { get; set; }
+        public DrawerItem SelectedItem { get; set; }
+        public IEnumerable<DrawerItem> Data { get; set; } =
+            new List<DrawerItem>
+            {
+            new DrawerItem
+            {
+                Text = "Home",
+                Icon = "home",
+                Url = "/"
+            },
+            //new DrawerItem { Separator = true},
+            new DrawerItem
+            {
+                Text = "AllPost",
+                Icon = "paperclip",
+                Url = "/AllPost"
+            },
+            //new DrawerItem { Separator = true},
+            new DrawerItem
+            {
+                Text = "addBlogPost",
+                Icon = "grid",
+                Url = "/addBlogPost"
+            },
+            //new DrawerItem { Separator = true},
+            new DrawerItem
+            {
+                Text = "PagedBlogPosts",
+                Icon = "globe",
+                Url = "/PagedBlogPosts"
+            },
+            //new DrawerItem { Separator = true},
+            new DrawerItem
+            {
+                Text = "register",
+                Icon = "hand",
+                Url = "/register"
+            },
+            //new DrawerItem { Separator = true},
+            new DrawerItem
+            {
+                Text = "login",
+                Icon = "login",
+                Url = "/login"
+            },
+            //new DrawerItem { Separator = true},
+            new DrawerItem
+            {
+                Text = "logout",
+                Icon = "logout",
+                Url = "/logout"
+            }
+            };
+
+        #endregion
+
+        #region Drawer Methods
+
+        protected override Task OnInitializedAsync()
+        {
+            SelectedItem = Data.First();
+
+            return base.OnInitializedAsync();
+        }
+
+        public async Task ToggleDrawer() => await Drawer.ToggleAsync();
+
+        #endregion
+    }
+}
