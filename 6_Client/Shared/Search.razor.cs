e2b29c0cf72c82547952b11af68c480b06b9c1fb
MohammadJavadTabari <92685218+MohammadJavadTabari@users.noreply.github.com>
2021-12-18 10:33:48 -0800

    cleaning and set timer for search

--- 6_Client/Shared/Search.razor.cs
+++ 6_Client/Shared/Search.razor.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Components;
+using System.Threading;
 
 namespace illegible.Client.Shared
 {
     public partial class Search
     {
+        private Timer _timer;
+
         public string SearchTerm { get; set; }
+
         [Parameter]
         public EventCallback<string> OnSearchChanged { get; set; }
+
+        private void SearchChanged()
+        {
+            if (_timer != null)
+                _timer.Dispose();
+
+            _timer = new Timer(OnTimerElapsed, null, 500, 0);
+        }
+
+        private void OnTimerElapsed(object sender)
+        {
+            OnSearchChanged.InvokeAsync(SearchTerm);
+            _timer.Dispose();
+        }
     }
 }
